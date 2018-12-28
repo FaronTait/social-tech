243c80242f2fdd1b1ca18b20f8c39905fb83ffde
Faron Tait <farontait@Farons-MacBook-Pro-7.local>
2018-12-28 12:41:18 +0200

    placeholder gamepage

--- social-tech/src/app/app-routing.module.ts
+++ social-tech/src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { PagesGamepageComponent} from "./components/pages/pages-gamepage/pages-gamepage.component";
+import { PagesHomepageComponent} from "./components/pages/pages-homepage/pages-homepage.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+
+  {path: 'homepage', component: PagesHomepageComponent},
+  {path: 'gamepage', component: PagesGamepageComponent}
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
