0d4d7a0c66a71cb9a3037bf7963475c3552887e9
Faron Tait <farontait@Farons-MacBook-Pro-7.local>
2019-01-04 17:29:49 +0200

    signup and endgame

--- social-tech/src/app/app.module.ts
+++ social-tech/src/app/app.module.ts
@@ -5,12 +5,16 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { PagesHomepageComponent} from "./components/pages/pages-homepage/pages-homepage.component";
 import { PagesGamepageComponent } from './components/pages/pages-gamepage/pages-gamepage.component';
+import { PagesSignuppageComponent } from './components/pages/pages-signuppage/pages-signuppage.component';
+import { PagesEndgamepageComponent } from './components/pages/pages-endgamepage/pages-endgamepage.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     PagesHomepageComponent,
-    PagesGamepageComponent
+    PagesGamepageComponent,
+    PagesSignuppageComponent,
+    PagesEndgamepageComponent
   ],
   imports: [
     BrowserModule,
