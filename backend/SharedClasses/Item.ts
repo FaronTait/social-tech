b7de3e6b89521b7e0b9c743d65bbfed576629db0
TRON <leonardvg@me.com>
2019-01-11 17:40:40 +0200

    I added svg, and priority attributes to the item class.

--- backend/SharedClasses/Item.ts
+++ backend/SharedClasses/Item.ts
@@ -2,9 +2,13 @@ export class Item {
     // TODO: Complete the definition of the item class.
     public value: number;
     public name: string;
+    public svg: string;
+    public priority: string;
 
-    constructor (name: string, value: number) {
+    constructor (name: string, value: number, svg: string, prioroty: string) {
         this.value = value;
         this.name = name;
+        this.svg = svg;
+        this.priority = prioroty;
     }
 }
