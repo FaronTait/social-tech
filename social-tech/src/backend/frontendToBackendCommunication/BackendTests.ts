6d3936b0aa407d879e248c6a50b4f3281538bd9d
TRON <leonardvg@me.com>
2019-01-11 18:02:55 +0200

    Fixed a compile error

--- social-tech/src/backend/frontendToBackendCommunication/BackendTests.ts
+++ social-tech/src/backend/frontendToBackendCommunication/BackendTests.ts
@@ -4,7 +4,7 @@ import {Item} from "../SharedClasses/Item";
 let itemArray: Item[] = [];
 
 for (let i = 0; i < 10; i++) {
-    itemArray[i] = new Item("I is Item " + i.toString(), 10 + i * 10, "tmp-id")
+    itemArray[i] = new Item('I is Item ' + i.toString(), 10 + i * 10, 'temp-id', 'tmp-svg', 'tmp-prioroty');
 }
 
  let testBackend = Backend.getInstance();
@@ -14,7 +14,7 @@ testBackend.setItemArray(itemArray);
  let newInfo = testBackend.getStartingInfo();
  for (let i = 0; i < 9; i++) {
      console.log("Wallet value: " + newInfo.newWalletValue.toString() + " Item name: " + newInfo.newItem.name + " Item value: " + newInfo.newItem.value.toString());
-     newInfo = testBackend.makeDecision(i % 2 == 1)
+     newInfo = testBackend.makeDecision(i % 2 == 1);
  }
 
  testBackend.getSessionDecisions();
