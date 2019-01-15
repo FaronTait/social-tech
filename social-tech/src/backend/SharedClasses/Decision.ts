48d104d2decab5ff3422eeb3075a75ce5dc52b20
TRON <leonardvg@me.com>
2019-01-15 22:45:14 +0200

    I made the Decision object's variables public.

--- social-tech/src/backend/SharedClasses/Decision.ts
+++ social-tech/src/backend/SharedClasses/Decision.ts
@@ -3,13 +3,13 @@ import {Item} from '../SharedClasses/Item';
 // This object was created according to the flowchart
 
 export class Decision {
-    private m_item: Item;
-    private m_wasBought: boolean;
-    private m_walletValueAtThatTime: number;
+    public item: Item;
+    public wasBought: boolean;
+    public walletValueAtThatTime: number;
 
     constructor(item: Item, wasBought: boolean, walletValueAtThatTime: number) {
-        this.m_item = item;
-        this.m_wasBought = wasBought;
-        this.m_walletValueAtThatTime = walletValueAtThatTime;
+        this.item = item;
+        this.wasBought = wasBought;
+        this.walletValueAtThatTime = walletValueAtThatTime;
     }
 }
